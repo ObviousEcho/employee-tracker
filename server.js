d1747bb889534025cc35ab2305714fd56b8843e9
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2022-10-08 11:56:53 -0600

    installed express package, created server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+require('dotenv').config();
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    },
+    console.log(`Connected to the employee_db database!`)
+);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
